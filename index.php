2692858812b3e84110aae9d6707e1b8925157594
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-24 16:57:57 +0100

    edit db file

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BedWords</title>
-</head>
-<body>
-    <form action="./test-db/index.php" method="GET">
-        <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
-        <input type="text" name="bedWord" id="">
-        <input type="submit" value="invia">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BedWords</title>
+    </head>
+    <body>
+        <form action="./test-db/index.php" method="GET">
+            <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
+            <input type="text" name="bedWord" id="">
+            <input type="submit" value="invia">
 
-    </form>
-</body>
+        </form>
+    </body>
 </html>
--- test-db/index.php
+++ test-db/index.php
@@ -1,29 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DB RESPONSE</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo $_GET['paragraph'];
+            ?>
+        </h1>
+        <h1>
+            Il tuo paragrafo e' lungo:
+            <?php
+                echo strlen($_GET['paragraph']);
+            ?>
+            caratteri (spazi inclusi)
+        </h1>
 
-<h1>
-    <?php
-        echo $_GET['paragraph'];
-    ?>
-</h1>
-<h1>
-    Il tuo paragrafo e' lungo:
-    <?php
-        echo strlen($_GET['paragraph']);
-    ?>
-     caratteri (spazi inclusi)
-</h1>
 
-
-<h2>
-    <?php
-        $toReplace = $_GET['bedWord'];
-        $newPar = str_replace($toReplace, '***', $_GET['paragraph']);
-        echo $newPar
-    ?>
-</h2>
-<h2>
-    Il tuo paragrafo e' lungo:
-    <?php
-        echo strlen($newPar);
-    ?>
-     caratteri (spazi inclusi)
-</h2>
+        <h2>
+            <?php
+                $toReplace = $_GET['bedWord'];
+                $newPar = str_replace($toReplace, '***', $_GET['paragraph']);
+                echo $newPar
+            ?>
+        </h2>
+        <h2>
+            Il tuo paragrafo e' lungo:
+            <?php
+                echo strlen($newPar);
+            ?>
+            caratteri (spazi inclusi)
+        </h2>
+    </body>
+</html>
